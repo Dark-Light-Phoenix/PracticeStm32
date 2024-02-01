95647ae9862ef7874b54a41ea3baf96eb488081a
Dark-Light-Phoenix <155775130+Dark-Light-Phoenix@users.noreply.github.com>
2024-02-01 17:13:01 +0200

    Resolve conflicts

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,14 +96,14 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+   while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
     if (HAL_GPIO_ReadPin (GPIOB, GPIO_PIN_7) == GPIO_PIN_RESET)
     {
-      HAL_TIM_Base_Stop(&htim10);
+    	
     }
     else
     {
